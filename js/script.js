a1082682c2d3c982449c66bb9733409714fb9bf6
San1aAnwar <152646616+San1aAnwar@users.noreply.github.com>
2024-05-02 01:31:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function downloadFile() {
+    window.location.href = '';
+  }
+
+
+const eventDate = new Date(2024, 4, 20); 
+
+function updateCountdown() {
+  const currentDate = new Date();
+  const timeDifference = eventDate - currentDate;
+
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+
+  document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+
+setInterval(updateCountdown, 1000);
+
+
+
+
+updateCountdown();
+
+window.addEventListener('load', function() {
+  document.querySelector('.loader').style.display = 'none';
+});
