6dbfe826237f38174955bfcac2c4a94919604d0c
San1aAnwar <152646616+San1aAnwar@users.noreply.github.com>
2024-05-02 21:34:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,14 @@ updateCountdown();
 window.addEventListener('load', function() {
   document.querySelector('.loader').style.display = 'none';
 });
+
+
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector(".content").style.visibility = "hidden";
+        document.querySelector(".loader").style.visibility = "visible";
+    } else {
+        document.querySelector(".loader").style.display = "none";
+        document.querySelector(".content").style.visibility = "visible";
+    }
+};
